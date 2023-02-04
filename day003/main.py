600f162953756566a209e0ee52f04761f131e1aa
Prince Radebe <princeradebe@gmail.com>
2023-02-04 06:32:21 +0200

    Day 3

--- day003/main.py
+++ day003/main.py
@@ -1,3 +1,17 @@
 #Prince Radebe
 #03 February 2023
 # 100 Days of Python
+
+#Logical Operators
+# A and B - both A and B must be true
+# A or B - either A or B must be true
+# not A - A must be false
+
+A = True
+B = True
+
+if A and B:
+    print("Both A and B are true")
+
+else:
+    print("Either A or B is false")
