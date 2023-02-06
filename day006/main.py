24a3d98d16f07dc73d9e8a27211be6f487558a97
Prince Radebe <princeradebe@gmail.com>
2023-02-06 23:54:31 +0200

    Day 6, learning about functions and while loops

---
+++ day006/main.py
@@ -0,0 +1,17 @@
+# Prince Radebe
+# DAY 006
+# 06 February 2023
+# 100 Days of Python
+
+# Functions, Code Blocks and While Loops
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+
+# my_function()
