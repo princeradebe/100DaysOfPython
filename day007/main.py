94e38886ee1e2217957ad0b96945d94354bf8e32
Prince Radebe <princeradebe@gmail.com>
2023-02-09 19:25:06 +0200

    Day 7, done a day later..... Hangman Game

--- day007/main.py
+++ day007/main.py
@@ -1,16 +1,61 @@
 # Hangman Game
 
+# import random
+# word_list = ["aardvark", "baboon", "camel"]
+
+# chosen_word = random.choice(word_list)
+
+# # Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+# guess = input("Guess a letter: ").lower()
+
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+
+# Step 5
+
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_words import word_list
+from hangman_art import logo
 
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-# Testing code
+end_of_game = False
+lives = 6
+
+
+print(logo)
+
 print(f'Pssst, the solution is {chosen_word}.')
-guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    print(f"{' '.join(display)}")
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    from hangman_art import stages
+    print(stages[lives])
