25ccbeac5ff1f3968c136847934218fa6605a857
Prince Radebe <princeradebe@gmail.com>
2023-02-13 23:36:12 +0200

    Fix the errors

--- day013/main.py
+++ day013/main.py
@@ -24,16 +24,18 @@
 # print(dice_imgs[dice_num])
 
 # # Play Computer
-year = int(input("What's your year of birth?"))
-if year > 1980 and year <= 1994:
-    print("You are a millenial.")
-elif year > 1994:
-    print("You are a Gen Z.")
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year <= 1994:
+#     print("You are a millenial.")
+# elif year > 1994:
+#     print("You are a Gen Z.")
 
 # # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+else:
+    print(f"You cannot drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
